799ef1b44e99975a1d46d39f693a0b73fbc303c4
Wang <contact@volt.id>
2023-03-07 12:13:42 +0800

    tokensale

--- OTAuction.sol
+++ OTAuction.sol
@@ -16,8 +16,8 @@ contract OTAuction is Ownable {
         uint256 startAt;
         uint256 endAt;
         uint256 otSupply;
-        uint256 floorFDV;
-        uint256 capFDV;
+        uint256 floorValue;
+        uint256 capMKTValue;
         uint256 totalReceived;
     }
 
@@ -31,6 +31,7 @@ contract OTAuction is Ownable {
     address[] public acceptedCoin;
     mapping(address => bool) public acceptedCoinExist;
     bool public withdrawn;
+    bool public allowClaim = false;
     mapping(address => uint256) public acceptedCoinBalance;
     // user value
     mapping(address => Position) public userPosition;
@@ -42,19 +43,19 @@ contract OTAuction is Ownable {
         _;
     }
 
-    modifier onlyAtEndTime(){
-        // solhint-disable-next-line not-rely-on-time
-        require(block.timestamp > saleInfo.endAt, "onlyAtEndTime");
-        _;
-    }
+    // modifier onlyAtEndTime(){
+    //     // solhint-disable-next-line not-rely-on-time
+    //     require(block.timestamp > saleInfo.endAt, "Not ended");
+    //     _;
+    // }
 
     event BuyOT(
         address account,
         address token,
         uint256 tokenAmount,
         uint256 otSupply,
-        uint256 floorFDV,
-        uint256 capFDV,
+        uint256 floorValue,
+        uint256 capMKTValue,
         uint256 totalReceived
     );
 
@@ -64,22 +65,22 @@ contract OTAuction is Ownable {
         uint256 _startAt, 
         uint256 _endAt,
         uint256 _otSupply, 
-        uint256 _floorFDV,
-        uint256 _capFDV,
+        uint256 _floorValue,
+        uint256 _capMKTValue,
         address[] memory _acceptedCoin
     ) {
         // solhint-disable-next-line not-rely-on-time
         require(block.timestamp < _startAt, "StartAt must future");
         require(_startAt < _endAt, "EndAt must gt StartAt");
-        require(_capFDV >= _floorFDV, "capFDV must gt floorFDV");
+        require(_capMKTValue >= _floorValue, "capMKTValue must gt floorValue");
         saleInfo.OT = IERC20(_OT);
         saleInfo.approver = _approver;
         saleInfo.startAt = _startAt;
         saleInfo.endAt = _endAt;
         saleInfo.otSupply = _otSupply;
-        // _floorFDV = usd  floorFDV = usd * (10 ** 30)
-        saleInfo.floorFDV = _floorFDV * 1e30;
-        saleInfo.capFDV = _capFDV * 1e30;
+        // _floorValue = usd  floorValue = usd * (10 ** 30)
+        saleInfo.floorValue = _floorValue * 1e30;
+        saleInfo.capMKTValue = _capMKTValue * 1e30;
         withdrawn = false;
         for (uint i = 0; i < _acceptedCoin.length; i++) {
             acceptedCoin.push(_acceptedCoin[i]);
@@ -88,9 +89,13 @@ contract OTAuction is Ownable {
         }
     }
 
-    function setEndTime(uint256 _endAt) external onlyOwner{
-        require(saleInfo.totalReceived == saleInfo.capFDV, "Cap not filled");
-        saleInfo.endAt = _endAt;
+    // function setEndTime(uint256 _endAt) external onlyOwner{
+    //     require(saleInfo.totalReceived == saleInfo.capMKTValue, "Cap not filled");
+    //     saleInfo.endAt = _endAt;
+    // }
+
+    function _allowClaim() external onlyOwner{
+        allowClaim = true;
     }
 
     function buyOT(address _address, uint256 _amount) external onlyAtSaleTime {
@@ -100,7 +105,7 @@ contract OTAuction is Ownable {
         userPosition[msg.sender].value += _value;
         saleInfo.totalReceived += _value;
         
-        require(saleInfo.totalReceived <= saleInfo.capFDV, "cap is filled");
+        require(saleInfo.totalReceived <= saleInfo.capMKTValue, "cap is filled");
         userPositonStable[msg.sender][_address] += _amount;
         acceptedCoinBalance[_address] += _amount;
         emit BuyOT(
@@ -108,20 +113,21 @@ contract OTAuction is Ownable {
             _address, 
             _amount, 
             saleInfo.otSupply, 
-            saleInfo.floorFDV, 
-            saleInfo.capFDV, 
+            saleInfo.floorValue, 
+            saleInfo.capMKTValue, 
             saleInfo.totalReceived
         );
     }
 
-    function claimOT() external onlyAtEndTime {
+    function claimOT() external {
         require(!userPosition[msg.sender].claimed, "You have already claimed");
+        require(allowClaim, "Not allowed to claim");
         userPosition[msg.sender].claimed = true;
         uint256 volume;
-        if (saleInfo.totalReceived > saleInfo.floorFDV) {
+        if (saleInfo.totalReceived > saleInfo.floorValue) {
             volume = saleInfo.totalReceived;
         } else {
-            volume = saleInfo.floorFDV;
+            volume = saleInfo.floorValue;
         }
         uint256 boughtOTAmount = userPosition[msg.sender].value * saleInfo.otSupply / volume;
         saleInfo.OT.safeTransferFrom(saleInfo.approver, msg.sender, boughtOTAmount);
@@ -129,7 +135,7 @@ contract OTAuction is Ownable {
 
     function withdrawProceeds(address to) external onlyOwner {
         require(!withdrawn, "withdrawn");
-        require(block.timestamp > saleInfo.endAt || saleInfo.totalReceived == saleInfo.capFDV, "cap not filled");
+        require(block.timestamp > saleInfo.endAt || saleInfo.totalReceived == saleInfo.capMKTValue, "cap not filled");
         withdrawn = true;
         for (uint i = 0; i < acceptedCoin.length; i++) {
             IERC20(acceptedCoin[i]).transfer(to, acceptedCoinBalance[acceptedCoin[i]]);
@@ -139,8 +145,8 @@ contract OTAuction is Ownable {
     function getAccountInfo(address _account) external view returns (
         address OTAddress,
         uint256 otSupply,
-        uint256 floorFDV,
-        uint256 capFDV,
+        uint256 floorValue,
+        uint256 capMKTValue,
         uint256 totalReceived,
         uint256 boughtOTAmount, 
         address[] memory tokens,
@@ -150,14 +156,14 @@ contract OTAuction is Ownable {
     ) {
         OTAddress = address(saleInfo.OT);
         otSupply = saleInfo.otSupply;
-        floorFDV = saleInfo.floorFDV;
-        capFDV = saleInfo.capFDV;
+        floorValue = saleInfo.floorValue;
+        capMKTValue = saleInfo.capMKTValue;
         totalReceived = saleInfo.totalReceived;
         uint256 volume;
-        if (saleInfo.totalReceived > saleInfo.floorFDV) {
+        if (saleInfo.totalReceived > saleInfo.floorValue) {
             volume = saleInfo.totalReceived;
         } else {
-            volume = saleInfo.floorFDV;
+            volume = saleInfo.floorValue;
         }
         boughtOTAmount = userPosition[_account].value * saleInfo.otSupply / volume;
         tokens = new address[](acceptedCoin.length);
